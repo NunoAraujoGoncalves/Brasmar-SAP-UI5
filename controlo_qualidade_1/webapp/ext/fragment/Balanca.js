fd2381a9cdf21d58ed501c8dff8c2aeeadff97c7
speeddragon2 <nunoaraujo1996@gmail.com>
2026-03-12 10:10:00 +0000

    Save configs da balança

--- controlo_qualidade_1/webapp/ext/fragment/Balanca.js
+++ controlo_qualidade_1/webapp/ext/fragment/Balanca.js
@@ -18,25 +18,37 @@ sap.ui.define([
             const oRowCtx = oEvent.getSource().getBindingContext();
             const oModel = oRowCtx.getModel();
             var pb = parseFloat(oRowCtx.getProperty("Pb"));
-            pb += parseFloat("1.30");
-            oRowCtx.setProperty("Pb", pb.toFixed(3));
-            MessageToast.show(oResourceBundle.getText("pesoBalanca"));
 
             //para debug usar about://device-log
             // Let user pick the COM/USB-serial device
-            if (1 === 2) {
-                navigator.serial.requestPort().then(async (result) => {
-                    var port = result;
-                    await port.open({ baudRate: 9600 });
+            _getPort().then((result) => {
+                var port = result;
+                port.open({
+                    baudRate: 9600,
+                    dataBits: 8,
+                    stopBits: 1,
+                    parity: "none",
+                    flowControl: "hardware"
+                }).then(async (result) => {
                     const textDecoder = new TextDecoderStream();
                     const readableClosed = port.readable.pipeTo(textDecoder.writable);
                     const reader = textDecoder.readable.getReader();
 
+                    const signals = await port.getSignals();
+                    console.log(signals);
+
                     try {
                         while (true) {
                             const { value, done } = await reader.read();
+                            console.log("done: " + done + ",value: " + value)
                             if (done) break;
-                            if (value) log(value);
+                            if (_isFloatString(value)) {
+                                console.log(value);
+                                pb = parseFloat(value);
+                                oRowCtx.setProperty("Pb", pb.toFixed(3));
+                                reader.cancel();
+                                MessageToast.show(oResourceBundle.getText("pesoBalanca"));                                
+                            }
                         }
                     } catch (e) {
                         log('Error: ' + e);
@@ -49,7 +61,30 @@ sap.ui.define([
                 }).catch((err) => {
                     MessageToast.show(err);
                 });
+            }).catch((err) => {
+                MessageToast.show(err);
+            });
+
+            async function _getPort() {
+                var ports = await navigator.serial.getPorts();
+                if (ports) {
+                    return ports[1];
+                } else {
+                    return await navigator.serial.requestPort();
+                }
             }
+
+            function _isFloatString(str) {
+                if (typeof str !== 'string') return false;
+                const s = str.trim();
+                if (s === '') return false;              // reject empty
+
+                const n = Number(s);
+                if (Number.isNaN(n)) return false;       // not numeric at all
+
+                return !Number.isInteger(n);            // true = non‑integer number (float)
+            }
+
         }
     };
 });
