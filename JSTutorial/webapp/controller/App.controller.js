9138b45127b3474c01a6803c5abe536d351e3aff
speeddragon2 <nunoaraujo1996@gmail.com>
2026-03-23 10:06:37 +0000

    Added JSTutorial

---
+++ JSTutorial/webapp/controller/App.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], (Controller, MessageToast,JSONModel,ResourceModel) => {
+    "use strict";
+
+    return Controller.extend("ui5.walkthrough.controller.App", {
+    });
+});  
