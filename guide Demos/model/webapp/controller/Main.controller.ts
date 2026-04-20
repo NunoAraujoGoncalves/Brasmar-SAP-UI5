fc88ba5a44fc1b0ed63957c646f6586ebc1888f1
speeddragon2 <nunoaraujo1996@gmail.com>
2026-04-20 10:21:09 +0100

    added model project

---
+++ guide Demos/model/webapp/controller/Main.controller.ts
@@ -0,0 +1,11 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace brasmar.teste.model.controller
+ */
+export default class Main extends Controller {
+
+    public onInit(): void {
+
+    }
+}
