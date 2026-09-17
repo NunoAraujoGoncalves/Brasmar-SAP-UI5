c872577ccf0bd028ea3eed07da55b4b63b4f7741
speeddragon2 <nunoaraujo1996@gmail.com>
2026-09-17 12:52:45 +0100

    Course SAP Press

---
+++ Rheinwerk/customerapp/webapp/controller/App.controller.ts
@@ -0,0 +1,11 @@
+import Controller from "sap/ui/core/mvc/Controller";
+
+/**
+ * @namespace com.sappress.customerapp.controller
+ */
+export default class App extends Controller {
+
+    public onInit(): void {
+
+    }
+}
